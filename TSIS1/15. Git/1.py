6c8006376fb8a6e360c721f92caaf3fc9b5783bb
Dosymzhan <d_birzhanuly@kbtu.kz>
2023-01-31 21:40:16 +0600

    TSIS1

---
+++ TSIS1/15. Git/1.py
@@ -0,0 +1,21 @@
+"""
+git init
+
+git config --global user.name "Dosymzhan"
+
+git config --global user.email "d_birzhanuly"
+
+git status
+
+touch README.md
+
+git init
+
+git add README.md
+
+git commit -m "TSIS"
+
+git remote add origin https://github.com/Dos004/pp2-22B21B1066.git
+
+git push -u origin master
+"""
