e59bb8575267016d974466cd8478b9bc5ef7cc53
Dosymzhan <d_birzhanuly@kbtu.kz>
2023-03-23 23:57:40 +0600

    TSIS2 folder commit

--- TSIS7/1.py
+++ TSIS7/1.py
@@ -1,13 +1,33 @@
-import os
-
-print(os.getcwd())
-tsis7=os.path.join(os.getcwd(),"TSIS7")
-os.chdir(tsis7)
-print(os.getcwd())
-
-for i in range(2,11):
-    name = str(i) + ".py"
-    with open(name,"w") as file:
-        file.write(" ")
-
-
+import pygame
+pygame.init()
+W, H = 800, 800
+x = W//2
+y = H//2
+WHITE = (255, 255, 255)
+sc = pygame.display.set_mode((W, H))
+clock = pygame.time.Clock()
+mickey = pygame.image.load(r"C:\Users\diase\OneDrive\Рабочий стол\Python\PP2\week 7\main-clock.png")
+leftHand = pygame.image.load(r"C:\Users\diase\OneDrive\Рабочий стол\Python\PP2\week 7\left-hand.png")
+rightHand = pygame.image.load(r"C:\Users\diase\OneDrive\Рабочий стол\Python\PP2\week 7\right-hand.png")
+mickeyRect = mickey.get_rect()
+def blitRotateCenter(surf, image, center, angle):
+    rotated_image = pygame.transform.rotate(image, angle)
+    new_rect = rotated_image.get_rect(center = image.get_rect(center = center).center)
+    surf.blit(rotated_image, new_rect)
+langle = 0
+rangle = 0
+while True:
+    sc.fill(0)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            exit()
+    langle -= 0.06
+    rangle -= 0.000006
+    
+    sc.fill(WHITE)
+    sc.blit(mickey, (x, y))
+    sc.blit(mickey, mickeyRect)
+    blitRotateCenter(sc, rightHand, (x,y), rangle)
+    blitRotateCenter(sc, leftHand, (x,y), langle) 
+    
+    pygame.display.update()
--- TSIS7/3.py
+++ TSIS7/3.py
@@ -1,1 +1,47 @@
- 
+import pygame
+
+# pygame setup
+pygame.init()
+w = 1280
+h = 720
+r = 25
+screen = pygame.display.set_mode((w,h))
+clock = pygame.time.Clock()
+running = True
+
+ball_x = w // 2
+ball_y = h // 2
+
+
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        
+    screen.fill("white")
+
+    pygame.draw.circle(screen, "red", (ball_x, ball_y), r)
+
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_UP]:
+            ball_y -= 20
+    if keys[pygame.K_DOWN]:
+            ball_y += 20
+    if keys[pygame.K_LEFT]:
+            ball_x -= 20
+    if keys[pygame.K_RIGHT]:
+            ball_x += 20
+
+    if ball_x < r:
+            ball_x = r
+    elif ball_x > w - r:
+            ball_x = w - r
+    if ball_y < r:
+            ball_y = r
+    elif ball_y > h - r:
+            ball_y = h - r
+
+    pygame.display.flip()
+    clock.tick(60)
+
+pygame.quit()
